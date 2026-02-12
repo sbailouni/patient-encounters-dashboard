1e480c36756aea0a4c9f27b687d0f28db6f3d186
Sara Bailouni <sbailouni@ucsd.edu>
2026-02-11 18:52:18 -0800

    Add Encounter interface for encounter data

---
+++ types/encounter.ts
@@ -0,0 +1,16 @@
+//Defines interface for an Encounter object
+
+/* Helpful for type checking data from JSON
+   files or backend API (if incorportated in future). */
+
+export interface Encounter {
+    id: string;
+    patientName: string;
+    patientId: string;
+    date: string;
+    type: string;
+    status: "completed" | "pending" | "cancelled";
+    provider: string;
+    chiefComplaint: string;
+    notes: string;
+}
